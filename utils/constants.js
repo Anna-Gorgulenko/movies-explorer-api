4303277eed2fe7b491353d1613e09b62363073e6
Anna Gorgulenko <a2767373@yandex.ru>
2023-07-12 21:49:32 +0300

    Для имени базы данных используется bitfilmsdb

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,5 @@
 // Адрес подключения к базе данных MongoDB
-const URL = 'mongodb://127.0.0.1:27017/moviesdb';
+const URL = 'mongodb://127.0.0.1:27017/bitfilmsdb';
 
 // Секретный ключ для разработки и отладки приложения
 const SECRET_KEY_DEV = 'dev-secret';
